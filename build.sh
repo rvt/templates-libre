82f404add4091999a4f915ddba3fb67f1a8ec952
Ries van Twisk <ries@librecad.org>
2011-05-20 07:36:01 -0500

    Added README text Update build.sh Added 'theme wide' example theme Modified repository

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-mkdir target
-mkdir target/native2ascii
+
 mvn install war:exploded jahia:deploy
+
+
+# Create a new templateset for Jahia
+# mvn archetype:generate -DarchetypeCatalog=http://maven.jahia.org/maven2
